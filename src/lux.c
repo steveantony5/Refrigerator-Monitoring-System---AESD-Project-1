922e5182a018dc107e111fcac398cde69c165884
steveantony5 <steveantony5@gmail.com>
2019-03-28 00:20:20 -0700

    added lux measurement func

--- src/lux.c
+++ src/lux.c
@@ -8,6 +8,7 @@
 #include <fcntl.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <math.h>
 
 #define CONTROL_REGISTER (0X00)
 #define TIMING_REGISTER (0X01)
@@ -22,6 +23,7 @@ int i2c_setup(int ,int );
 int lux_sensor_setup();
 int read_channel_0();
 int read_channel_1();
+float lux_measurement(float , float );
 
 int file_des;
 char bus[15];
@@ -35,7 +37,7 @@ uint8_t LSB_1;
 uint16_t CH0;
 uint16_t CH1;
 
-uint16_t ratio;
+float lux;
 
 
 int main()
@@ -69,9 +71,8 @@ int main()
 		printf("CH0 %d\n",CH0);
 		printf("CH1 %d\n",CH1);
 
-		ratio = (CH1 / CH0);
-
-		printf("ratio %d\n",ratio);
+		lux = lux_measurement(CH0,CH1);
+		printf("lux %f\n",lux);
 
 	}
 }
@@ -227,3 +228,32 @@ int read_channel_1()
 }
 
 
+float lux_measurement(float CH0, float CH1)
+{
+
+	float ratio = (CH1 / CH0);
+
+	//0 < CH1/CH0 ≤ 0.50 Sensor Lux = (0.0304 x CH0) – (0.062 x CH0 x ((CH1/CH0)1.4))
+
+	if((ratio <=0.5)&& (ratio > 0))
+		return ((0.0304 * CH0) - (0.062 * CH0 * (powf(ratio, 1.4))));
+
+	//0.50 < CH1/CH0 ≤ 0.61 Sensor Lux = (0.0224 x CH0) – (0.031 x CH1)
+
+	else if((ratio  > 0.5)&& (ratio <= 0.61))
+		return ((0.0224 * CH0) - (0.031 * CH1));
+
+    //0.61 < CH1/CH0 ≤ 0.80 Sensor Lux = (0.0128 x CH0) – (0.0153 x CH1)
+	else if((ratio  > 0.61)&& (ratio <= 0.8))
+		return (0.0128 * CH0) - (0.0153 * CH1);
+
+	//0.80 < CH1/CH0 ≤ 1.30 Sensor Lux = (0.00146 x CH0) – (0.00112 x CH1)
+	else if((ratio  > 0.80)&& (ratio <= 1.30))
+		return (0.00146 * CH0) - (0.00112 * CH1);
+
+	//CH1/CH0>1.30 Sensor Lux = 0
+	else if(ratio  > 1.30)
+		return 0;
+
+
+}
